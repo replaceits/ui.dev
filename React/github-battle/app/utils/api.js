391a5bb7319774e634cea3b4304fafd06e8aef29
replaceits <sid@sidw.me>
2021-08-19 23:54:04 -0400

    Export function properly

--- React/github-battle/app/utils/api.js
+++ React/github-battle/app/utils/api.js
@@ -1,5 +1,5 @@
 
-const fetchPopularRepos = language => {
+export const fetchPopularRepos = language => {
   const endpoint = window.encodeURI(`https://api.github.com/search/repositories?q=stars:>1+language:${language}&sort=stars&order=desc&type=Repositories`)
 
   return fetch(endpoint)

926257a897a56717513bd371fabb61e3543f1c48
replaceits <sid@sidw.me>
2021-08-20 01:53:18 -0400

    Add Battle component

---
+++ React/github-battle/app/components/Battle.js
@@ -0,0 +1,35 @@
+
+import React from 'react';
+import { FaUser, FaFighterJet, FaTrophy, FaUserFriends } from 'react-icons/fa';
+
+const Instructions = () => (
+  <div className='instructions-container'>
+    <h1 className='center-text header-lg'>Instructions</h1>
+    <ol className='container-sm grid center-text battle-instructions'>
+      <li>
+        <h3 className='header-sm'>Enter two Github users</h3>
+        <FaUserFriends className='bg-light' color='rgb(255, 191, 116)' size={140} />
+      </li>
+      <li>
+        <h3 className='header-sm'>Battle</h3>
+        <FaFighterJet className='bg-light' color='#727272' size={140} />
+      </li>
+      <li>
+        <h3 className='header-sm'>See the winners</h3>
+        <FaTrophy className='bg-light' color='rgb(255, 215, 0)' size={140} />
+      </li>
+    </ol>
+  </div>
+);
+
+class Battle extends React.Component {
+  render() {
+    return (
+      <React.Fragment>
+        <Instructions />
+      </React.Fragment>
+    )
+  }
+}
+
+export default Battle;

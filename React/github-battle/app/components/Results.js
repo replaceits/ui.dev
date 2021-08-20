0850d0080974f99f96802c6e16223366c2d6e098
replaceits <sid@sidw.me>
2021-08-20 12:41:56 -0400

    Output results to DOM

--- React/github-battle/app/components/Results.js
+++ React/github-battle/app/components/Results.js
@@ -3,18 +3,37 @@ import React from 'react';
 import {battle} from '../utils/api';
 
 class Results extends React.Component {
+  state = {
+    winner: null,
+    loser: null,
+    error: null,
+    loading: true
+  }
+
   componentDidMount() {
     const {playerOne, playerTwo} = this.props;
 
     battle([playerOne, playerTwo])
-      .then(players => console.log('data: ', players));
+      .then(players => {
+        this.setState({
+          winner: players[0],
+          loser: players[1],
+          error: null,
+          loading: false
+        });
+      }).catch(({message}) => {
+        this.setState({
+          error: message,
+          loading: false
+        })
+      });
   }
 
   render() {
     return (
       <div>
         Results
-        <pre>{JSON.stringify(this.props)}</pre>
+        <pre>{JSON.stringify(this.state)}</pre>
       </div>
     )
   }

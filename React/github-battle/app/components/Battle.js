f98e3ee30c5ec9eacc1493b2a965f9d6c09edb4d
replaceits <sid@sidw.me>
2021-08-20 02:45:32 -0400

    Add player inputs

--- React/github-battle/app/components/Battle.js
+++ React/github-battle/app/components/Battle.js
@@ -78,10 +78,41 @@ PlayerInput.propTypes = {
 }
 
 class Battle extends React.Component {
+  state = {
+    playerOne: null,
+    playerTwo: null
+  }
+
+  handleSubmit(id, player) {
+    this.setState({
+      [id]: player
+    });
+  }
+
   render() {
+    const {playerOne, playerTwo} = this.state;
+
     return (
       <React.Fragment>
         <Instructions />
+
+        <div className='players-container'>
+          <h1 className='center-text header-lg'>Players</h1>
+          <div className='row space-around'>
+            {playerOne === null && (
+              <PlayerInput
+                label='Player One'
+                onSubmit={player => this.handleSubmit('playerOne', player)}
+              />
+            )}
+            {playerTwo === null && (
+              <PlayerInput
+                label='Player Two'
+                onSubmit={player => this.handleSubmit('playerTwo', player)}
+              />
+            )}
+          </div>
+        </div>
       </React.Fragment>
     )
   }

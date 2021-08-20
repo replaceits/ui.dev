56828c89be784081053c56fd4876db0c96bb2da6
replaceits <sid@sidw.me>
2021-08-19 22:44:38 -0400

    Add propTypes to navbar

--- React/github-battle/app/components/Popular.js
+++ React/github-battle/app/components/Popular.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 const LanguagesNav = ({selected, onUpdateLanguage}) => (
   <ul className='flex-center'>
@@ -15,6 +16,11 @@ const LanguagesNav = ({selected, onUpdateLanguage}) => (
     ))}
   </ul>
 );
+
+LanguagesNav.propTypes = {
+  selected: PropTypes.string.isRequired,
+  onUpdateLanguage: PropTypes.func.isRequired
+};
 
 const languages = [
   'All',

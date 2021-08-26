546393c646114b9592339e8369e0c198f99706cc
replaceits <sid@sidw.me>
2021-08-26 00:23:05 -0400

    Add navbar to switch between components

--- React/github-battle/app/components/Nav.js
+++ React/github-battle/app/components/Nav.js
@@ -1,5 +1,10 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 import { ThemeConsumer } from '../contexts/theme';
+
+const activeStyle = {
+  color: 'rgb(187, 46, 31)'
+};
 
 class Nav extends React.Component {
   render() {
@@ -7,6 +12,27 @@ class Nav extends React.Component {
       <ThemeConsumer>
         {({theme, toggleTheme})=>(
           <nav className='row space-between'>
+            <ul className='row nav'>
+              <li>
+                <NavLink 
+                  to ='/'
+                  exact
+                  activeStyle={activeStyle}
+                  className='nav-link'
+                >
+                  Popular
+                </NavLink>
+              </li>
+              <li>
+                <NavLink 
+                  to ='/battle' 
+                  activeStyle={activeStyle}
+                  className='nav-link'
+                >
+                  Battle
+                </NavLink>
+              </li>
+            </ul>
             <button
               style={{fontSize: 30}}
               className='btn-clear'

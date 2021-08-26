ee7e4497cbf6e8741410d801de333bc1e2aec50a
replaceits <sid@sidw.me>
2021-08-25 22:30:00 -0400

    Add theme provider/consumer

--- React/github-battle/app/index.js
+++ React/github-battle/app/index.js
@@ -3,14 +3,26 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Popular from './components/Popular';
 import Battle from './components/Battle';
+import { ThemeProvider } from './contexts/theme';
 import './index.css';
 
 class App extends React.Component {
+  state = {
+    theme: 'light',
+    toggleTheme: () => {
+      this.setState(({theme}) => ({
+        theme: theme === 'light' ? 'dark' : 'light'
+      }))
+    }
+  }
+
   render() {
     return (
-      <div className="container">
-        <Battle />
-      </div>
+      <ThemeProvider value={this.state}>
+        <div className="container">
+          <Battle />
+        </div>
+      </ThemeProvider>
     );
   }
 }
